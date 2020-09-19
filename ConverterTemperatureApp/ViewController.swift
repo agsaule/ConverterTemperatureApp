874e06271dbe146b661e0d26954f4ec7953c5a87
agsaule <agsaule@hotmail.com>
2020-09-19 11:28:10 +0300

    t converter app

--- ConverterTemperatureApp/ViewController.swift
+++ ConverterTemperatureApp/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    
+    @IBOutlet weak var tempSlider: UISlider! {
+        didSet{
+            tempSlider.minimumValue = 0
+            tempSlider.maximumValue = 99
+            tempSlider.value = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        fahrenheitLabel.text = "32°"
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        print("value: ", sender.value)
+        let tempCelsius = Int(sender.value)
+        celsiusLabel.text = "\(tempCelsius) °C"
+        
+        let tempFahr = Int((sender.value * 9 / 5) + 32)
+        fahrenheitLabel.text = "\(tempFahr) °F"
+    }
 }
 
